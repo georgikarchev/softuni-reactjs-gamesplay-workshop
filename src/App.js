a3810b748c23d79ce71e5bb0efe13776451417f7
Georgi Karchev <georgi.karchev@gmail.com>
2022-10-26 11:07:41 +0300

    Add navigation links now load the corresponding pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import { Header } from "./Components/Header/Header";
 import { Home } from "./Components/Home/Home";
+import { Login } from "./Components/Login/Login";
+// import { Logout } from "./Components/Logout/Logout";
+import { Register } from "./Components/Register/Register";
+import { Edit } from "./Components/Edit/Edit";
+import { Create } from "./Components/Create/Create";
+import { Catalogue } from "./Components/Catalogue/Catalogue";
 
+ 
 import "./App.css";
 
 function App() {
@@ -13,6 +20,12 @@ function App() {
       <main id="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/logout" element={<Logout />} /> */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/catalogue" element={<Catalogue />} />
         </Routes>
         
       </main>
