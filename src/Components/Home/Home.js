57bfa0966f1f8e2b17caa86466649267abb6fb7c
Georgi Karchev <georgi.karchev@gmail.com>
2022-10-26 09:55:21 +0300

    Render latest games conditionally or show message if no games are available

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -22,8 +22,11 @@ export const Home = () => {
       <img src="./images/four_slider_img01.png" alt="hero" />
       <div id="home-page">
         <h1>Latest Games</h1>
-        {games.length && games.map( (x,i) => <LatestGame key={x.title} game={x} />)}
-        {!games.length && <p className="no-articles">No games yet</p>}
+
+          {games.length > 0
+            ? games.map( (x,i) => <LatestGame key={x._id} game={x} />)
+            : <p className="no-articles">No games yet</p>
+          }
       </div>
     </section>
   );
