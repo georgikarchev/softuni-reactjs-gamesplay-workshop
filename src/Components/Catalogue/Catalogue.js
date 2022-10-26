4d94a146be285ea415263e567cfbc600faca6e89
Georgi Karchev <georgi.karchev@gmail.com>
2022-10-26 12:03:20 +0300

    Add Catalogue & CatalogueItem
    Catalogue component generates a dynamic list of CatalogueItems.

--- src/Components/Catalogue/Catalogue.js
+++ src/Components/Catalogue/Catalogue.js
@@ -1,38 +1,15 @@
-export const Catalogue = () => {
+import { CatalogueItem } from "./CatalogueItem/CatalogueItem";
+
+export const Catalogue = ({ games }) => {
   return (
     <section id="catalog-page">
       <h1>All Games</h1>
       {/* Display div: with information about every game (if any) */}
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="./images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>Cover Fire</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="./images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>Zombie lang</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="./images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>MineCraft</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
+
+      {games.map((x) => (
+        <CatalogueItem key={x._id} game={x} />
+      ))}
+
       {/* Display paragraph: If there is no games  */}
       <h3 className="no-articles">No articles yet</h3>
     </section>
