e322e270c2e6b5069c25fe893d63475e1c08a7d0
Georgi Karchev <georgi.karchev@gmail.com>
2022-10-26 12:06:14 +0300

    Display Message in Catalogue Page if No Games Found

--- src/Components/Catalogue/Catalogue.js
+++ src/Components/Catalogue/Catalogue.js
@@ -4,14 +4,15 @@ export const Catalogue = ({ games }) => {
   return (
     <section id="catalog-page">
       <h1>All Games</h1>
-      {/* Display div: with information about every game (if any) */}
-
-      {games.map((x) => (
-        <CatalogueItem key={x._id} game={x} />
-      ))}
-
-      {/* Display paragraph: If there is no games  */}
-      <h3 className="no-articles">No articles yet</h3>
+      {
+        games.length > 0
+          ? (
+            games.map((x) => <CatalogueItem key={x._id} game={x} />)
+          )
+          : (
+            <h3 className="no-articles">No articles yet</h3>
+          )
+      }
     </section>
   );
 };
